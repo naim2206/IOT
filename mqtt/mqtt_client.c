574fad86bd5a7703014e9ab2361bf56cab7298b7
ntowfigh <ntowfigh@oracle.com>
2024-04-15 19:14:41 -0600

    mqtt

--- mqtt/mqtt_client.c
+++ mqtt/mqtt_client.c
@@ -7,16 +7,21 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define PORT 3000
-#define TIMES 5
-#define SIZE 100
+#define PORT 1883
 
 
-struct connectComand
+typedef struct
 {
-    int keepAlive;
-    int data;
-}
+    uint8_t bFrameType;
+    uint16_t wMsgLen;
+    uint16_t wProtlNameLen;
+    char sProtName[4];
+    uint8_t bVersion;
+    uint8_t bConnectFlags;
+    uint16_t bKeepAlive;
+    uint16_t wClientIdLen;
+    char sClientID[100];
+} sConnect;
 
 int main(int argc, char const* argv[])
 {
@@ -24,23 +29,30 @@ int main(int argc, char const* argv[])
 
     int sock = socket(AF_INET, SOCK_STREAM, 0);
 
-    *struct connectComand connect;
-    connect = malloc(sizeof(struct connectComand));
+    *sConnect connect = malloc(sizeof(struct connectComand));
 
     memset(&addr, 0, sizeof(addr));
 
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    inet_pton(AF_INET, "192.168.1.17", &(client.sin_addr));
     addr.sin_port = htons(PORT);
     addr.sin_family = AF_INET;
 
     int ret = connect(sock, (const struct sockaddr*)&addr, sizeof(addr));
     // send connect command
-    connect->keepAlive = 1;
-    connect->data = 1;
+    connect->bFrameType = 0x10;
+    connect->wMsgLen = 0x0006;
+    connect->wProtlNameLen = 0x0004;
+    connect->sProtName = "MQTT";
+    connect->bVersion = 0x04;
+    connect->bConnectFlags = 0x02;
+    connect->bKeepAlive = 0x000A;
+    connect->wClientIdLen = 0x0004;
+    connect->sClientID = "test";
+
+    char buff[1024];
     ret = send(sock, (void*)connect, sizeof(struct connectComand), 0);
-    ret = recv(sock, (void*)buff, SIZE, 0);
-    printf("Rx: %d\n", ret);
-    printf("%s", buff);
+    ret = recv(sock, buff, SIZE, 0);
+    printf("%X", buff);
 
     ret = close(sock);
 
